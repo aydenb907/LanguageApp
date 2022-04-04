5b8c75a7484a9c36c1c9c7e344b079e0ff8da899
Ayden Boyd <aydboyd@chbc.school.nz>
2022-04-05 09:37:28 +1200

    Marks questions and words correctly

--- LanguageApp/Lesson.cs
+++ LanguageApp/Lesson.cs
@@ -8,12 +8,12 @@ namespace LanguageApp
 {
     class Lesson
     {
-        private static List<string> practiceQues = new List<string>() { "question 1", "question 2", "question 3" };
-        private static List<string> practiceAns = new List<string>() { "answer 1", "answer 2", "answer 3" };
-        private List<string> englishWords;
-        private List<string> germanWords;
-        private List<string> testQues;
-        private List<string> testAns;
+        private static List<string> practiceQues = new List<string>() { "question 1", "question 2", "question 3", "question 4", "question 5" };
+        private static List<string> practiceAns = new List<string>() { "answer 1", "answer 2", "answer 3", "answer 4", "answer 5" };
+        private static List<string> englishWords = new List<string>() { "e1", "e2", "e3", "e4", "e5" };
+        private static List<string> germanWords = new List<string>() { "g1", "g2", "g3", "g4", "g5" };
+        private static List<string> testQues = new List<string>() { "question 1", "question 2", "question 3", "question 4", "question 5" };
+        private static List<string> testAns = new List<string>() { "answer 1", "answer 2", "answer 3", "answer 4", "answer 5" };
 
 
         public static List<string> GenPracQuestions(List<int> randomIndex)
@@ -23,25 +23,61 @@ namespace LanguageApp
             foreach(int index in randomIndex)
             {
                 chosenQuest.Add(practiceQues[index]);
+              
             }
 
             return chosenQuest;
         }
-        public static List<string> GenPracAnswers(List<int> randomIndex)
+
+        public static List<string> GenPracAns(List<int> randomIndex)
         {
             List<string> chosenAns = new List<string>();
 
-            foreach(int index in randomIndex)
+            foreach (int index in randomIndex)
             {
                 chosenAns.Add(practiceAns[index]);
+
             }
 
             return chosenAns;
         }
 
-        public static bool MarkPracQuestions(int index, string userAnswer)
+
+        public static bool MarkPracQuestions(int index, string userAnswer, List<string> chosenAnswers)
         {
-            if(userAnswer.Equals(practiceAns[index]))
+            if(userAnswer.Equals(chosenAnswers[index]))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public static List<string> GenEnglishWords(List<int> randomIndex)
+        {
+            List<string> chosenWords = new List<string>();
+
+            foreach (int index in randomIndex)
+            {
+                chosenWords.Add(englishWords[index]);
+            }
+
+            return chosenWords;
+        }
+        public static List<string> GenGermanWords(List<int> randomIndex)
+        {
+            List<string> chosenWords = new List<string>();
+
+            foreach (int index in randomIndex)
+            {
+                chosenWords.Add(germanWords[index]);
+            }
+
+            return chosenWords;
+        }
+
+        public static bool MarkTranslations(int index, string userAnswer, List<string> chosenGermanWords)
+        {
+            if (userAnswer.Equals(chosenGermanWords[index]))
             {
                 return true;
             }
@@ -49,5 +85,36 @@ namespace LanguageApp
             return false;
         }
 
+        public static List<string> GenTestQuestions(List<int> randomIndex)
+        {
+            List<string> chosenQuest = new List<string>();
+
+            foreach (int index in randomIndex)
+            {
+                chosenQuest.Add(testQues[index]);
+            }
+
+            return chosenQuest;
+        }
+        public static List<string> GenTestAnswers(List<int> randomIndex)
+        {
+            List<string> chosenAns = new List<string>();
+
+            foreach (int index in randomIndex)
+            {
+                chosenAns.Add(testAns[index]);
+            }
+
+            return chosenAns;
+        }
+        public static bool MarkTestQues(int index, string userAnswer, List<string> chosenAnswers)
+        {
+            if (userAnswer.Equals(chosenAnswers[index]))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
--- LanguageApp/Program.cs
+++ LanguageApp/Program.cs
@@ -12,60 +12,31 @@ namespace LanguageApp
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-       
-      
+
+
         static void Main()
         {
-        
+
             //Adds new user's details: name and password
             string name = "fjfjr";
             string password = "34";
             List<int> scores = new List<int>();
 
-        
+
             User u = new User();
             u.NewUser(name, password, scores);
 
-            //Gives what lesson it is
-            //int lesson = 1;
 
-            int score = 0;
             //Generates practice questions in random order based on what lesson it is
             Random rand = new Random();
             List<int> randomIndexes = new List<int>();
-            for(int i=0; i<5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                randomIndexes.Add(rand.Next(0,3));
+                randomIndexes.Add(rand.Next(0, 4));
             }
 
             List<string> practiceQues = Lesson.GenPracQuestions(randomIndexes);
-            List<string> practiceAns = Lesson.GenPracQuestions(randomIndexes);
-
-           for(int j=0; j<5; j++)
-            {
-                Console.WriteLine(practiceQues[j]);
-
-                string userAnswer = "answer 2";
-
-                if (Lesson.MarkPracQuestions(j, userAnswer))
-                {
-                
-                    Console.WriteLine("Correct");
-                }
-                else
-                {
-                    Console.Write("Incorrect");
-                }
-            }
-
-            randomIndexes = new List<int>();
-
-            for (int i = 0; i < 5; i++)
-            {
-                randomIndexes.Add(rand.Next(0, 3));
-            }
-
-            List<string> germanWords = Lesson.GenPracQuestions(randomIndexes);
+            List<string> practiceAns = Lesson.GenPracAns(randomIndexes);
 
             for (int j = 0; j < 5; j++)
             {
@@ -73,20 +44,80 @@ namespace LanguageApp
 
                 string userAnswer = "answer 2";
 
-                if (Lesson.MarkPracQuestions(j, userAnswer))
+                //Marks practice questions
+                if (Lesson.MarkPracQuestions(j, userAnswer, practiceAns))
                 {
-                    score++;
+
                     Console.WriteLine("Correct");
                 }
                 else
                 {
-                    Console.Write("Incorrect");
+                    Console.Write("Incorrect\n");
                 }
             }
 
+            //Generates English words to translate into German
+            randomIndexes = new List<int>();
 
+            for (int i = 0; i < 5; i++)
+            {
+                randomIndexes.Add(rand.Next(0, 3));
+            }
 
+            List<string> englishWords = Lesson.GenEnglishWords(randomIndexes);
+            List<string> germanWords = Lesson.GenGermanWords(randomIndexes);
 
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine(englishWords[j]);
+
+                string userAnswer = "g2";
+
+                //Marks German translations
+                if (Lesson.MarkTranslations(j, userAnswer, germanWords))
+                {
+                    Console.WriteLine("Correct");
+                }
+                else
+                {
+                    Console.Write("Incorrect\n");
+                }
+            }
+
+            //Generates test questions
+            randomIndexes = new List<int>();
+            int score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                randomIndexes.Add(rand.Next(0, 3));
+
+            }
+
+            List<string> testQues = Lesson.GenTestQuestions(randomIndexes);
+            List<string> testAns = Lesson.GenTestAnswers(randomIndexes);
+
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine(testQues[j]);
+
+            }
+
+            List<string> userAnswers = new List<string>() { "answer 2", "answer 2", "answer 2", "answer 2", "answer 2" };
+
+            for (int j = 0; j < 5; j++)
+            {
+                //Marks test questions and adds score
+                if (Lesson.MarkTestQues(j, userAnswers[j], testAns))
+                {
+                    Console.WriteLine("Correct");
+                    score++;
+                }
+                else
+                {
+                    Console.Write("Incorrect\n");
+                }
+            }
 
             /*Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
